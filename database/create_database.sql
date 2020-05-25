b8f2770050e268e45798b4542864c467a6847a40
Alexandre Wery <alexbwery@gmail.com>
2020-05-25 19:47:55 +0200

    add Token table

--- database/create_database.sql
+++ database/create_database.sql
@@ -1,14 +1,18 @@
 CREATE TABLE `Utilisateur` (
-    `id` int NOT NULL,
+    `id` int NOT NULL AUTO_INCREMENT,
     `identifiant` varchar(255) NOT NULL,
     `email` varchar(255) DEFAULT NULL,
-    `hashpassword` varchar(255) DEFAULT NULL
+    `hashpassword` varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE (`identifiant`),
+    UNIQUE (`email`)
 );
 
-ALTER TABLE `Utilisateur`
-    ADD PRIMARY KEY (`id`),
-    ADD UNIQUE KEY `identifiant` (`identifiant`),
-    ADD UNIQUE KEY `email` (`email`);
-
-ALTER TABLE `Utilisateur`
-    MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+CREATE TABLE `Token` (
+    `token` VARCHAR(32) NOT NULL,
+    `service` int,
+    `id_user` int NOT NULL,
+    PRIMARY KEY(`token`, `service`),
+    UNIQUE (`id_user`),
+    FOREIGN KEY (`id_user`) REFERENCES `Utilisateur`(`id`)
+);
