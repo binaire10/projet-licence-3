461457bffa64464a50f65fe6bd1e61138d178a6d
Alexandre Wery <alexbwery@gmail.com>
2020-05-26 02:29:44 +0200

    token database model

---
+++ webserver/src/App/Models/TokenService.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Models;
+
+
+class TokenService extends \CodeIgniter\Model
+{
+    const LOGIN = 1;
+    protected $table      = 'Token';
+    protected $primaryKey = ['token', 'service'];
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = true;
+
+    protected $allowedFields = ['service', 'token', 'id_user'];
+
+    protected $useTimestamps = false;
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
+}
