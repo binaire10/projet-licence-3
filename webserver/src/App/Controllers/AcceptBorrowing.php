dbabd3b14f0a0da80d19fc18544896fc8b34e9d3
KIB00631 <kevin.ibares@gmail.com>
2020-05-29 03:59:20 +0200

    Fin contrôleur pour accepter ou refuser les demande d'emprunt/réservation.

--- webserver/src/App/Controllers/AcceptBorrowing.php
+++ webserver/src/App/Controllers/AcceptBorrowing.php
@@ -11,13 +11,47 @@ class AcceptBorrowing extends BaseController
     public function bookingRequests() {
         $db = \Config\Database::connect();
         return view('booking_list', [
-            'title' => 'Booking list',
-            'Booking requests' => $db->table('Reservation')->select('*')->get()->getResultArray()
+            'title' => 'booking list',
+            'bookingRequests' => $db->table('Reservation')->select('*')->get()->getResultArray()
         ]);
     }
-    public function cancelBooking () {
+    public function cancelBooking ()
+    {
+        if (!$this->isLibrarian)
+            throw new PageNotFoundException();
+        $userId = $this->request->getPost('userId');
+        $bookId = $this->request->getPost('bookId');
+        $action = $this->request->getPost('action');
+        if (isset($action, $userId, $bookId) && $action == 'cancel') {
+            $db = \Config\Database::connect();
+            $db->table('Reservation')->where('id_user', $userId)->where('id_livre', $bookId)->delete();
+            return view('booking_cancel_success', ['title' => 'Booking cancel successful']);
+        }
+        return view('booking_cancel', ['title' => 'Cancel this booking']);
+    }
+    public function confirmBooking () {
         if(!$this->isLibrarian)
             throw new PageNotFoundException();
+        $userId = $this->request->getPost('userId');
+        $bookId = $this->request->getPost('bookId');
+        $action = $this->request->getPost('action');
+        $idExmplaire = $this->request->getPost('idExemplaire');
+        $bookingDate = $this->request->getPost('bookingDate');
+        $dueDate = $this->request->getPost('dueDate');
+
+        if (isset($action, $userId, $bookId) && $action == 'confirm'){
+            $db = \Config\Database::connect();
+            $db->table('HistoriqueEmprunt')->insert([$idExmplaire,$bookId,$userId,$bookingDate,$dueDate]);
+            $db->table('EmpruntCourant')->insert([$idExmplaire,$bookId,$userId,$bookingDate]);
+            $db->table('Reservation')->where('id_user', $userId)->where('id_livre', $bookId)->delete();
+            return view('booking_success', ['title' => 'Booking successful']);
+        }
+        return view('booking_confirm',['title' => 'Confirm this booking']);
+
+
+
+
+
 
     }
 }
