03886aee720ebbd3389281dbe7d039493a65a9c7
Vincent malkowiak <vincent.malkowiak@iutnice.unice.fr>
2020-05-29 02:39:43 +0200

    merge branch correction

--- webserver/src/App/Config/App.php
+++ webserver/src/App/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://volk0s.alwaysdata.net/';
+	public $baseURL = 'http://localhost/';
 
 	/*
 	|--------------------------------------------------------------------------
